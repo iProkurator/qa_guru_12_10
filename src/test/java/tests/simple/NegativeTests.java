715c85a5e9e94eb4b65ad0bb945e5e0539682535
Pavel Pilatov <pilatov@gmail.com>
2022-05-14 00:45:09 +0300

    added simple tests

---
+++ src/test/java/tests/simple/NegativeTests.java
@@ -0,0 +1,20 @@
+package tests.simple;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class NegativeTests {
+
+    @Test
+    void test00() {
+        assertTrue(false);
+    }
+
+    @Test
+    void test01() {
+        assertTrue(false);
+    }
+
+
+}
