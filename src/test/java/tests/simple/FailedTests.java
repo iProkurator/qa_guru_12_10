ad7882532eaf2994f420b3eec7d69606f9610903
Pavel Pilatov <pilatov@gmail.com>
2022-05-14 03:00:43 +0300

    added allure attaches

--- src/test/java/tests/simple/FailedTests.java
+++ src/test/java/tests/simple/FailedTests.java
@@ -1,5 +1,6 @@
 package tests.simple;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -7,11 +8,13 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class FailedTests {
 
     @Test
+    @Disabled
     void test00() {
         assertTrue(false);
     }
 
     @Test
+    @Disabled
     void test01() {
         assertTrue(false);
     }
--- src/test/java/tests/simple/PositiveTests.java
+++ src/test/java/tests/simple/PositiveTests.java
@@ -1,5 +1,6 @@
 package tests.simple;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -7,21 +8,25 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class PositiveTests {
 
     @Test
+    @Disabled
     void test00() {
         assertTrue(true);
     }
 
     @Test
+    @Disabled
     void test01() {
         assertTrue(true);
     }
 
     @Test
+    @Disabled
     void test03() {
         assertTrue(true);
     }
 
     @Test
+    @Disabled
     void test04() {
         assertTrue(true);
     }
